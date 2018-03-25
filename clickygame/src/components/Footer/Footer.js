f3016e07898307c9a0cfa4d07f6cc6bc8e8b34c7
Ally Smith <allysmith424@gmail.com>
2018-03-25 17:46:35 -0500

    package.json changes for deployment

---
+++ clickygame/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+import reactLogo from "../../images/reactLogo.png"
+
+const Footer = props =>
+  <footer className="footer">
+    <img className="footerImg" src={reactLogo} alt="React" />
+  </footer>;
+
+export default Footer;
